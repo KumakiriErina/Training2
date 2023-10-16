46fdf588b454a59570ea7c14c01c0f898361f82b
KumakiriErina <146689246+KumakiriErina@users.noreply.github.com>
2023-10-16 11:16:34 +0900

    Update Fortune.java
    変更を行いました。
    変更点：javadocの記載、disp()

--- training2/Fortune.java
+++ training2/Fortune.java
@@ -1,11 +1,17 @@
 package training2;
 
 import java.util.ResourceBundle;
-
+/**
+ * Fortuneインターフェース
+ * @author e_kumakiri
+ */
 public interface Fortune {
-	//プロパティファイルの読み込み、値の取得（定数）
+	/** プロパティファイルの読み込み、値の取得(定数) */
 	String DISP_STR = ResourceBundle.getBundle("fortune").getString("disp_str");
 
-	//運勢結果の表示
+	/**
+	 * 運勢結果の表示（抽象メソッド）
+	 * @return　運勢
+	 */
 	String disp();
 }
