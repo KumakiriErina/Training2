24512dbef03ba7f68ba030fe5f9930624e6a60dc
KumakiriErina <146689246+KumakiriErina@users.noreply.github.com>
2023-10-16 11:17:19 +0900

    Update GoodBlessing.java
    変更を行いました。
    変更点：Javadocの記載。

--- training2/GoodBlessing.java
+++ training2/GoodBlessing.java
@@ -1,30 +1,11 @@
 package training2;
-
+/**
+ * 具象クラス（吉）
+ * @author e_kumakiri
+ */
 public class GoodBlessing extends Omikuji {
-	//吉
 	@Override
-	public void setUnsei(String unsei) {
-		this.unsei = unsei;
-	}
-	public void setNegaigoto(String negaigoto) {
-		this.negaigoto = negaigoto;
-	}
-	public void setAkinai(String akinai) {
-		this.akinai = akinai;
-	}
-	public void setGakumon(String gakumon) {
-		this.gakumon = gakumon;
-	}
-	public String getUnsei() {
-		return this.unsei;
-	}
-	public String getNegaigoto() {
-		return this.negaigoto;
-	}
-	public String getAkinai() {
-		return this.akinai;
-	}
-	public String getGakumon() {
-		return this.gakumon;
+	public void setUnsei() {
+		this.unsei = "吉";
 	}
 }
