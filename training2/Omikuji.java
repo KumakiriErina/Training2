08dee859b7bb5633bca4825eba5f4cbb522cb29f
KumakiriErina <146689246+KumakiriErina@users.noreply.github.com>
2023-10-16 11:19:51 +0900

    Update Omikuji.java
    変更を行いました。
    変更点；Javadocの記載、disp()、getterとsetter

--- training2/Omikuji.java
+++ training2/Omikuji.java
@@ -1,25 +1,68 @@
 package training2;
-
+/**
+ * おみくじ抽象クラス
+ * @author e_kumakiri
+ */
 public abstract class Omikuji implements Fortune {
+	/** 運勢を表す */
 	protected String unsei;
+	/** 願い事を表す */
 	protected String negaigoto;
+	/** 商いを表す */
 	protected String akinai;
+	/** 学問を表す */
 	protected String gakumon;
 
-	public abstract void setUnsei(String unsei);
-	public abstract void setNegaigoto(String negaigoro);
-	public abstract void setAkinai(String akinai);
-	public abstract void setGakumon(String gakumon);
-	
-	public abstract String getUnsei();
-	public abstract String getNegaigoto();
-	public abstract String getAkinai();
-	public abstract String getGakumon();
-
-	public String disp() {
-		String str = String.format(DISP_STR, this.unsei) + "\n" +"願い事:" +getNegaigoto() + "\n" + "商い:" + getAkinai() + "\n" + "学問:" + getGakumon();
-		System.out.println(str);
-		return str;
+	/** 各運勢クラスで運勢をsetする */
+	public abstract void setUnsei();
+	/**
+	 * @param negaigoto  願い事をsetする
+	 */
+	public void setNegaigoto(String negaigoto) {
+		this.negaigoto = negaigoto;
+	}
+	/**
+	 * @param akinai 商いをsetする
+	 */
+	public void setAkinai(String akinai) {
+		this.akinai = akinai;
+	}
+	/**
+	 * @param gakumon 学問をsetする
+	 */
+	public void setGakumon(String gakumon) {
+		this.gakumon = gakumon;
+	}
+	/**
+	 * @return 運勢
+	 */
+	public String getUnsei() {
+		return this.unsei;
+	}
+	/**
+	 * @return 願い事
+	 */
+	public String getNegaigoto() {
+		return this.negaigoto;
+	}
+	/**
+	 * @return 商い
+	 */
+	public String getAkinai() {
+		return this.akinai;
+	}
+	/**
+	 * @return 学問
+	 */
+	public String getGakumon() {
+		return this.gakumon;
 	}
 
+	/**
+	 * @return 運勢の表示
+	 */
+	public String disp() {
+		String str = String.format(DISP_STR, this.unsei) + "\n" +"願い事:" +getNegaigoto() + "\n" + "商い:" + getAkinai() + "\n" + "学問:" + getGakumon();
+		return str;
+	}
 }
