7a73e094afefc2e2f8cc564932b51d310478ae85
KumakiriErina <146689246+KumakiriErina@users.noreply.github.com>
2023-10-16 17:03:44 +0900

    Update Omikuji.java
    変更を行いました。
    変更点：Javadocの記載（説明文）、disp()のgetUnsei

--- training2/Omikuji.java
+++ training2/Omikuji.java
@@ -16,42 +16,49 @@ public abstract class Omikuji implements Fortune {
 	/** 各運勢クラスで運勢をsetする */
 	public abstract void setUnsei();
 	/**
+	 * 願い事をsetするためのメソッドです。
 	 * @param negaigoto  願い事をsetする
 	 */
 	public void setNegaigoto(String negaigoto) {
 		this.negaigoto = negaigoto;
 	}
 	/**
+	 * 商いをsetするためのメソッドです。
 	 * @param akinai 商いをsetする
 	 */
 	public void setAkinai(String akinai) {
 		this.akinai = akinai;
 	}
 	/**
+	 * 学問をsetするためのメソッドです。
 	 * @param gakumon 学問をsetする
 	 */
 	public void setGakumon(String gakumon) {
 		this.gakumon = gakumon;
 	}
 	/**
+	 * 運勢を取得するためのメソッドです。
 	 * @return 運勢
 	 */
 	public String getUnsei() {
 		return this.unsei;
 	}
 	/**
+	 * 願い事を取得するためのメソッドです。
 	 * @return 願い事
 	 */
 	public String getNegaigoto() {
 		return this.negaigoto;
 	}
 	/**
+	 * 商いを取得するためのメソッドです。
 	 * @return 商い
 	 */
 	public String getAkinai() {
 		return this.akinai;
 	}
 	/**
+	 * 学問を取得するためのメソッドです。
 	 * @return 学問
 	 */
 	public String getGakumon() {
@@ -59,10 +66,11 @@ public abstract class Omikuji implements Fortune {
 	}
 
 	/**
+	 * 運勢を表示するためのメソッドです。
 	 * @return 運勢の表示
 	 */
 	public String disp() {
-		String str = String.format(DISP_STR, this.unsei) + "\n" +"願い事:" +getNegaigoto() + "\n" + "商い:" + getAkinai() + "\n" + "学問:" + getGakumon();
+		String str = String.format(DISP_STR, getUnsei()) + System.lineSeparator() +"願い事:" +getNegaigoto() + System.lineSeparator() + "商い:" + getAkinai() + System.lineSeparator() + "学問:" + getGakumon();
 		return str;
 	}
 }
